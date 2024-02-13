7c6dd4ec6bce8e5c7c679540326ddbff11da83c6
dariaar <droso00@fesb.hr>
2024-02-13 15:51:59 +0100

    product css

--- semester-project/my-app/components/Product.tsx
+++ semester-project/my-app/components/Product.tsx
@@ -12,38 +12,41 @@ interface ProductProps {
 }
 
 const Product: React.FC<ProductProps> = ({ imageUrl, category, title, price, onAddToCart }) => {
-    const urlNew = title.trim().replaceAll(" ", "-");
+  const urlNew = title.trim().replaceAll(" ", "-");
 
-    return (
-        <div className="flex flex-col md:flex-row items-center w-fit h-fit mx-[15px]">
-          <div className="flex">
-            <div className="relative mt-[30px] mr-[30px]">
-                <Image
-                src={imageUrl}
-                alt={title}
-                width={400} 
-                height={400} 
-                />
-                <div className=" text-terra absolute w-full h-full -top-[30px] left-[30px] -z-10 bg-peach1"></div>
-            </div>
-            </div>
-    
-          <div className="flex flex-col">
-               
-                <p className="font-serif text-xl font-bold whitespace-break-spaces text-terra">
-                    {price}
-                </p>
-                <Link href={"/shop/" + category + "/" + urlNew}>
-                    <button
-                    className="font-serif text-dark_blue bg-peach1 hover:text-peach1 hover:bg-dark_blue p-[7px] m-[10px] rounded-sm"
-                    onClick={onAddToCart}
-                    >
-                    Add to cart
-                    </button>
-                </Link>
-          </div>
+  return (
+    <div className="flex flex-col items-center w-fit h-fit mx-[15px]">
+      
+
+      <div className="mb-3 rounded overflow-hidden">
+        <div className="relative">
+          <Image
+            src={imageUrl}
+            alt={title}
+            width={400} 
+            height={400} 
+            className="rounded-xl"
+          />
+          <div className="text-terra absolute w-full h-full -top-[30px] left-[30px] -z-10 bg-peach1"></div>
         </div>
-      );
-    };
-    
-    export default Product;
+      </div>
+
+      <p className="font-serif text-xl font-bold whitespace-break-spaces text-terra mb-3">
+        {price}
+      </p>
+
+      <div>
+        <Link href={"/shop/" + category + "/" + urlNew}>
+          <button
+            className="font-serif text-green2 bg-terra hover:text-terra hover:bg-peach p-[7px] m-[10px] rounded-xl"
+            onClick={onAddToCart}
+          >
+            Add to cart
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
